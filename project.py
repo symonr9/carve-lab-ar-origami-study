d70b2eb20bd715d3e1b251b7dc8f64959d46f44e
Symon <ramossy@oregonstate.edu>
2019-06-13 14:27:40 -0700

    Implemented xbox controller button support mapping

--- project.py
+++ project.py
@@ -1,5 +1,10 @@
 ﻿from setup import *
 
+
+####################################################################################
+### Function Declarations ##########################################################
+####################################################################################
+
 def saveResultsToDB():
 	print "Successfully saved results into database! ";
 	
@@ -12,38 +17,88 @@ def updateScreenText(origamiType, stepNum):
 		textOnScreen.message(swanInstructions[stepNum]);
 	textOnScreen.setScale(0.10,0.10,0.10);
 		
-def nextStep():
-	global origamiType, isStepCompleted, currentStepNum, taskData;
+def goToNextStep():
+	global origamiType, isStepCompleted, currentStepNum, onScreenStepNum, taskData;
 	
 	if currentStepNum != endStepNum:
 		#Continue to the next step.
 		taskData.startNextStep();
 		updateScreenText(origamiType, currentStepNum);
 		currentStepNum += 1;
-	
-		#taskData.printData();
+		onScreenStepNum = currentStepNum;
 	else:
 		print "You have completed this task!";
 		print "Please save results by pressing the spacebar";
-		taskData.updatedOverallTime();		
+		taskData.updateOverallTime();		
 		taskData.printData();
 		taskIsCompleted = True;
+		
+def goToPreviousStep():
+	global origamiType, isStepCompleted, currentStepNum, onScreenStepNum, taskData;
+	
+	if(currentStepNum > 0):
+		taskData.revertToLastStep();
+		updateScreenText(origamiType, currentStepNum - 1);
+		currentStepNum -= 1;
+		onScreenStepNum = currentStepNum;
 
+#Note: The keyboard press event for the numbers 0, 1, 2, and 3 all map to the 
+#xbox controller inputs. 
 def handleKeyPressEvent(key):
+	global origamiType, onScreenStepNum, currentStepNum; 
+	
 	if key == ' ':
 		if taskIsCompleted:
 			saveResultsToDB();
 	else:
 		if not taskIsCompleted:
-			#The user is still going through the current task.
-			nextStep();
+			if key == '0':
+				#The user is still going through the current task.
+				goToNextStep();
+			elif key == '1':
+				if(onScreenStepNum < currentStepNum):
+					onScreenStepNum += 1;
+					updateScreenText(origamiType, onScreenStepNum);
+			elif key == '2':
+				if(onScreenStepNum > 0):
+					onScreenStepNum -= 1;
+					updateScreenText(origamiType, onScreenStepNum);
+			elif key == '3':
+				goToPreviousStep();
 		else:
 			print "Please save results by pressing the spacebar.";
+			
+#green "a" button - buttonNum: 0 - Increments the current step to the next step.
+#red "b" button - buttonNum: 1 - Traverses to the next step (does not increment current step).
+#blue "x" button - buttonNum: 2 - Traverses to the previous step.
+#yellow "y" button - buttonNum: 3 - Reverts the current step to the previous step.
+def handleXboxButtonPressEvent(e):
+	handleKeyPressEvent(str(e.button));
 
-### Program Execution ##############
+
+####################################################################################
+####################################################################################
+####################################################################################
+
+
+####################################################################################
+### Program Execution ##############################################################
+####################################################################################
+
+#Initialize data recording object.
 taskData = OrigamiTimeData(origamiType);
 
+#Start vizard up.
 viz.go(viz.STEREO_HORZ);
+
+#Connect to the VideoVision cameras.
 cameras = VideoVision.add(camType=VideoVision.UEYE);
+
+#Setup callback functions to handle key and button press events.
 viz.callback(viz.KEYDOWN_EVENT,handleKeyPressEvent);
-####################################
+viz.callback(viz.SENSOR_DOWN_EVENT, handleXboxButtonPressEvent);
+
+
+####################################################################################
+####################################################################################
+####################################################################################
--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-﻿### Libraries #######################################################################
+﻿#####################################################################################
+### Libraries #######################################################################
+#####################################################################################
+
 import viz # general viz module
+import vizact
 import vizinfo # vizinfo is used along w/ vizinput to gather input throughout the program 
 import vizshape # vizshape is used for the creation and manipulation of the box sensors and tiles. 
 import math # vizmath is used for various mathematical operations used in the program.
@@ -20,9 +24,61 @@ from threading import Timer
 
 #import data dependency and project files.
 from config import * 
+
+#####################################################################################
+#####################################################################################
 #####################################################################################
 
-### Setup ###########################################################################
+
+####################################################################################
+######### Variables Declarations ###################################################
+####################################################################################
+
+#There are XX boat steps and YY swan steps, but an extra buffer step is added in order 
+#to programmatically account for the intermediate time between when the actual experiment 
+#begins and when the program is executed by the research assistant. 
+lastSwanStepNum = 10 + 1;
+lastBoatStepNum = 15 + 1;
+
+
+#Note that for both the boat and swan instructions, an extra element "You have completed this task!"
+#is included.
+
+boatInstructions = [
+	"1. position the paper vertically (top/bottom sides are shorter).",
+	"2. Fold the left side to the right side.",
+	"3. Unfold the left side from the right side.",
+	"4. Fold the top side to the bottom side.",
+	"5. Fold the top-left and top-right corners towards the center so that the two corners meet.",
+	"6. Fold the top layer of the bottom flap upwards and crease.",
+	"7. Turn the paper over. ",
+	"8. Fold the bottom flap upwards and crease.",
+	"9. Pull the two layers apart from the bottom and collapse it into a diamond.",
+	"10. Fold the top layer from the bottom corner to the top corner.",
+	"11. Turn the paper over. ",
+	"12. Fold the top layer from the bottom corner to the top corner.",
+	"13. Pull the two layers apart from the bottom and collapse it into a diamond.",
+	"14. Gently pull apart the two thin layers on the left and right sides.",
+	"15. Crease the triangle-shaped section and position it vertically.",
+	"You have completed this task!"
+];
+
+swanInstructions = [
+	"1. Position the paper like a diamond.",
+	"2. Fold the bottom-left and bottom-right sides to the center so that the sides meet.",
+	"3. Flip the paper over.",
+	"4. Fold the bottom-left and bottom-right sides to the center so that the sides meet.",
+	"5. Fold the bottom corner towards the top.",
+	"6. Fold down a small section from the top corner of the top layer.",
+	"7. Flip the paper over.",
+	"8. Fold the left side (both layers) to the right and crease.",
+	"9. Hold the bottom layer down and pull the top layer up.",
+	"10. Pull the small section of the top layer out.",
+	"You have completed this task!"
+];
+
+#All of these variables are subject to change based on user input. 
+#The RA will specify these variables when they launch the program.
 participantID = "";
 origamiType = "";
 methodType = "";
@@ -30,10 +86,20 @@ taskType = "";
 taskNum = "";
 setNum = "";
 
-#All of these variables are subject to change based on user input. 
-#The RA will specify these variables when they launch the program.
+currentStepNum = 0;
+onScreenStepNum = 0;
+endStepNum = 0;
+taskIsCompleted = False;
 
-#FIXME TODO: See if it should be an integer or a string in the database.
+currentStep = "Step " + str(currentStepNum);
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+
+#####################################################################################
+### User Input ######################################################################
+#####################################################################################
 
 participantID = raw_input("Please enter participant ID: ");
 
@@ -71,8 +137,7 @@ elif setNum == "2":
     elif taskNum == "3":
         taskType = "Third3";
     
-
-#Output User Input that the RA provides
+#Print User Input that the RA provides
 print "Participant ID: ", participantID;
 print "Origami Type: ", origamiType;
 print "Method Type: ", methodType;
@@ -82,6 +147,16 @@ if origamiType == "Swan":
 	endStepNum = lastSwanStepNum;
 elif origamiType == "Boat":
 	endStepNum = lastBoatStepNum;
+
+####################################################################################
+####################################################################################
+####################################################################################
+
+
+
+####################################################################################
+### Vizard Text Initialization #####################################################
+####################################################################################
 
 #5/22/19: Resolved bug where certain text dissapears when attempting to reference textOnScreen object inside of 
 #key event functions. Each letter that was missing was correlated to the letter not being in the starting text. 
@@ -102,11 +177,20 @@ textOnScreen.resolution(1);
 viz.mouse.setVisible(viz.OFF); # remove mouse cursor when program starts
 viz.phys.enable();   
 
+# Load DirectInput plug-in for xbox controller interface.
+dinput = viz.add('DirectInput.dle');
+joy = dinput.addJoystick();
+joy.setDeadZone(0.2);
+
+####################################################################################
+####################################################################################
 ####################################################################################
 
 
-
+####################################################################################
 ### StopWatch class declaration ####################################################
+####################################################################################
+
 class OrigamiTimeData: 
     def __init__(self, origamiType):
         self.origamiType = origamiType;
@@ -139,7 +223,7 @@ class OrigamiTimeData:
         self.stepTimes = [0] * (lastBoatStepNum + 1);
         
         self.overallTime = 0;
-    def updatedOverallTime(self):
+    def updateOverallTime(self):
         self.overallTime = 0;
         
         #Subtract from the initial grace period time before the timer officially started.
@@ -155,9 +239,11 @@ class OrigamiTimeData:
         #total time elapsed for a particular step.
         self.stepTimes[self.currentStepNum] = self.currentStepEndTime - self.currentStepStartTime;
         
-        self.updatedOverallTime();
+        self.updateOverallTime();
         
         self.currentStepNum += 1;
+    def revertToLastStep(self):
+        self.currentStepNum -= 1;
     def printData(self):
         print "Origami Type: ";
         print self.origamiType;
@@ -171,4 +257,6 @@ class OrigamiTimeData:
             print "Step ", str(t), ": ";
             print self.stepTimes[t];
     
+####################################################################################
+####################################################################################
 ####################################################################################
